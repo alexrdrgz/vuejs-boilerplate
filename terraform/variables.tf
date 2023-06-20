d576d8d2372dae2add131bad0dff021a85d200ec
Alejandro Rodriguez <alejandro.xsc@gmail.com>
2023-06-20 10:17:59 -0700

    doc(variables.tf): added description to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "domain_name" {
   type        = string
-  description = "The domain name for the site"
+  description = "The domain name for the site, this can be a root domain or subdomain"
 }
 
 variable "route53_zone" {
   type = string
-  description = "the name of the domain that you have purchased on aws route53"
+  description = "the name of the domain that you have purchased on aws route53, should just be something like example.com"
 }
 
 variable "access_key" {
