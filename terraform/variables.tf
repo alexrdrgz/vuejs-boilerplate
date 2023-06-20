6125f5a8524f62415f472771b9f305482d44533a
Alejandro Rodriguez <alejandro.xsc@gmail.com>
2023-06-19 23:05:02 -0700

    feat(variables.tf): created variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the site"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket"
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
